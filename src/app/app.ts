a42c18bf8160c33494cdbe20b592541a536a0892
sreemaye <sreemaye44@gmail.com>
2023-12-11 12:22:39 +0600

    initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,6 @@
-import express, { Request, Response } from 'express';
-import cors from 'cors';
-const app = express();
-
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+const app: Application = express();
 
 //parsers
 app.use(express.json());
@@ -9,7 +8,7 @@ app.use(cors());
 
 //application routes
 
-app.get("/", (req:Request, res:Response) => {
+app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
