f9c64a495b4b19f52c1ea5693cd8cf5d40e8d894
sreemaye <sreemaye44@gmail.com>
2023-12-18 17:27:17 +0600

    level, tags are fixed

--- src/app/helpers/filterHelper.ts
+++ src/app/helpers/filterHelper.ts
@@ -14,7 +14,7 @@ export const filter = <T>(
     "sortOrder",
     "fields",
   ];
-  const { minPrice, maxPrice, ...restQuery } = queryObj;
+  const { minPrice, maxPrice, sortBy, tags, level, ...restQuery } = queryObj;
   excludedFields.forEach((keyword) => delete queryObj[keyword]);
 
   modelQuery = modelQuery.find(restQuery);
@@ -23,6 +23,31 @@ export const filter = <T>(
   }
   if (maxPrice !== undefined) {
     modelQuery = modelQuery.find({ price: { $lte: maxPrice } });
+  }
+  if (tags !== undefined) {
+    modelQuery = modelQuery.find({
+      "tags.name": tags,
+      "tags.isDeleted": false,
+    });
+  }
+  if (level !== undefined) {
+    modelQuery = modelQuery.find({ "details.level": level });
+  }
+
+  if (sortBy !== undefined) {
+    const allowedSortFields = [
+      "title",
+      "price",
+      "startDate",
+      "endDate",
+      "language",
+      "durationInWeeks",
+    ];
+
+    if (allowedSortFields.includes(sortBy)) {
+      const sortOrder = queryObj.sortOrder === "desc" ? -1 : 1;
+      modelQuery = modelQuery.sort({ [sortBy]: sortOrder });
+    }
   }
 
   return modelQuery;
